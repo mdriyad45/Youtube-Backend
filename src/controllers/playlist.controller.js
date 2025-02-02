335c73980cf9d99fc3f5cc066999de0e982250d5
mdriyad45 <jakariyakabirriyad@gmail.com>
2025-02-02 21:48:34 +0600

    create playlist

---
+++ src/controllers/playlist.controller.js
@@ -0,0 +1,9 @@
+export const createPlaylist = async ( req, res)=>{
+    try {
+        const {name, description} = req.body;
+
+        if(![name, description].some())
+    } catch (error) {
+        
+    }
+}
