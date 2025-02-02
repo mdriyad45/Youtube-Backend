9748f67f4228375df3ce69bd97b4c182e6abfb8c
mdriyad45 <jakariyakabirriyad@gmail.com>
2025-02-02 23:10:54 +0600

    fix create playlist

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,9 +1,33 @@
-export const createPlaylist = async ( req, res)=>{
-    try {
-        const {name, description} = req.body;
+import { apiError } from "../utils/apiError";
+import { Playlist } from "../models/playlist.model";
 
-        if(![name, description].some())
-    } catch (error) {
-        
+export const createPlaylist = async (req, res) => {
+  try {
+    const { name, description } = req.body;
+
+    if (!(name, description)) {
+      throw new apiError(400, "name and description must required");
     }
-}
+    const playlist = await Playlist.create({
+      name,
+      description,
+      owner: req.user?._id,
+    });
+    if (!playlist) {
+      throw new apiError(400, "faild to create playlist");
+    }
+
+    res.status(200).json({
+      message: "playlist created successfully",
+      data: playlist,
+      success: true,
+      error: false,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+      success: false,
+      error: true,
+    });
+  }
+};
