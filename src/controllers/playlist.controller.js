fc106b4a1fb1a048dc0866dba4ce31d6780bce77
mdriyad45 <jakariyakabirriyad@gmail.com>
2025-02-03 01:15:58 +0600

    create updatePlaylist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -32,3 +32,51 @@ export const createPlaylist = async (req, res) => {
     });
   }
 };
+
+export const updatePlaylist = async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    if (!(name, description)) {
+      throw new apiError(400, "name and description must required!");
+    }
+    const playlistId = req.params._playlistId;
+
+    if (!playlistId) {
+      throw new apiError(400, "playlistId not found");
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if (!playlist) {
+      throw new apiError(400, "playlist not found");
+    }
+    if (playlist.owner.tostring() !== req.user?._id.tostring()) {
+      throw new apiError("only owner can edit the playlist");
+    }
+
+    const updatePlaylist = await Playlist.findByIdAndUpdate(
+      playlistId,
+      {
+        $set: {
+          name,
+          description,
+        },
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      message: "Playlist update successfully",
+      data: updatePlaylist,
+      success: true,
+      error: false,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      message: error.message,
+      success: false,
+      error: true,
+    });
+  }
+};
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import {
+  createPlaylist,
+  updatePlaylist,
+} from "../controllers/playlist.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/create-playlist").post(authMiddleware, createPlaylist);
+router
+  .route("/update-playlist/:_playlistId")
+  .post(authMiddleware, updatePlaylist);
 
 export default router;
