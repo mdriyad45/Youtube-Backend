8c641e887e34b275df6802d326d9585b667e1536
mdriyad45 <jakariyakabirriyad@gmail.com>
2025-02-03 22:04:37 +0600

    create controller getPlaylistById

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,4 +1,4 @@
-import { isValidObjectId } from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Playlist } from "../models/playlist.model.js";
 import { apiError } from "../utils/apiError.js";
 
@@ -118,4 +118,87 @@ export const deletePlaylist = async ( req, res) =>{
             error: true
         })
     }
+}
+
+export const getPlaylistById = async ( req, res) => {
+
+  const playlistId = req.params._playlistId;
+
+  if(!isValidObjectId(playlistId)){
+    throw new apiError(400, "Invalid playlistId");
+  }
+  
+  const playlist = await Playlist.findById(playlistId);
+
+  if(!playlist){
+    throw new apiError(400, "This playlis is not exist");
+  }
+
+  const playlistVideos = Playlist.aggregate([
+    {
+      $match:{
+        _id: new mongoose.Types.ObjectId(playlistId)
+      }
+    },
+    {
+      $lookup:{
+        from: 'videos',
+        localField: 'videos',
+        foreignField: '_id',
+        as: 'videos'
+      }
+    },
+    {
+      $match: {
+        "videos.isPublished": true,
+      }
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'owner',
+        foreignField: '_id',
+        as: "owner"
+      }
+    },
+    {
+      $addFields: {
+        totalVideos:{
+          $size: '$videos'
+        },
+        totalViews: {
+          $sum: '$videos.views'
+        },
+        owner: {
+          $first: '$owner'
+        }
+      }
+    },
+    {
+      $project:{
+        name: 1,
+        description: 1,
+        createdAt:1,
+        updatedAt: 1,
+        videos: {
+          _id:1,
+          'videoFile.secure_url': 1,
+          'thumbnail.secure_url':1,
+          title:1,
+          description: 1,
+          duration: 1,
+          views: 1,
+          createdAt:1,
+          updated:1
+        },
+        owner:{
+          _id:1,
+          username:1,
+          fullname:1,
+          avatar:1,
+          
+        }
+      }
+    }
+  ])
 }
