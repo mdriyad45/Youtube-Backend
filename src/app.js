a15b9562a0fa3cdf2e5168caf720b22dfdcb735a
mdriyad45 <jakariyakabirriyad@gmail.com>
2025-06-03 21:59:27 +0600

    update code

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
-import { rateLimit } from 'express-rate-limit'
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 
@@ -14,16 +14,16 @@ app.use(
 );
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-})
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+});
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(helmet());
-app.use(limiter)
+app.use(limiter);
 
 //import routes
 
@@ -38,8 +38,8 @@ app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/playlist", playlist);
 app.use("/api/v1/comment/", comment);
 app.use("/api/v1/like/", like);
-app.use('/', (req, res)=>{
+app.use("/", (req, res) => {
   res.send("Welcome to youtube Api");
-})
+});
 
 export default app;
